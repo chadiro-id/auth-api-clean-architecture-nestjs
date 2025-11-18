54d23b514b8c6bcc5088a204b31b57e945d317cc
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-11-19 00:38:06 +0700

    Remove id property

--- src/domain/entities/account-contact.ts
+++ src/domain/entities/account-contact.ts
@@ -4,26 +4,12 @@ export enum ContactType {
 }
 
 export class AccountContact {
-  public readonly id: string;
-  public readonly accountId: string;
-  public readonly contactType: ContactType;
-  public readonly contactValue: string;
-  public readonly isPrimary: boolean;
-  public readonly verifiedAt: Date;
-
   constructor(
-    id: string,
-    accountId: string,
-    contactType: ContactType,
-    contactValue: string,
-    isPrimary: boolean,
-    verifiedAt: Date,
-  ) {
-    this.id = id;
-    this.accountId = accountId;
-    this.contactType = contactType;
-    this.contactValue = contactValue;
-    this.isPrimary = isPrimary;
-    this.verifiedAt = verifiedAt;
-  }
+    public readonly accountId: string,
+    public readonly contactType: ContactType,
+    public readonly contactValue: string,
+    public readonly isPrimary: boolean,
+    public readonly verifiedAt: Date | null | undefined,
+    public readonly createdAt: Date,
+  ) {}
 }
