50a96069fcf659e2b3b7f0f2f3c2e529af103b78
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-11-18 15:09:43 +0700

    Add key property

--- src/domain/entities/account-provider.ts
+++ src/domain/entities/account-provider.ts
@@ -5,6 +5,7 @@ export enum ProviderType {
 
 export class AccountProvider {
   public readonly id: string;
+  public readonly accountId: string;
   public readonly providerType: ProviderType;
   public readonly providerId: string;
   public readonly credentials: string | null | undefined;
@@ -12,12 +13,14 @@ export class AccountProvider {
 
   constructor(
     id: string,
+    accountId: string,
     providerType: ProviderType,
     providerId: string,
     credentials: string | null | undefined,
     createdAt: Date,
   ) {
     this.id = id;
+    this.accountId = accountId;
     this.providerType = providerType;
     this.providerId = providerId;
     this.credentials = credentials;
