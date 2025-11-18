d15a774124226e979212f246e7ad1c4e3fa3447a
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-11-18 14:43:18 +0700

    New: account provider entity

---
+++ src/domain/entities/account-provider.ts
@@ -0,0 +1,26 @@
+export enum ProviderType {
+  EMAIL,
+  GOOGLE,
+}
+
+export class AccountProvider {
+  public readonly id: string;
+  public readonly providerType: ProviderType;
+  public readonly providerId: string;
+  public readonly credentials: string | null | undefined;
+  public readonly createdAt: Date;
+
+  constructor(
+    id: string,
+    providerType: ProviderType,
+    providerId: string,
+    credentials: string | null | undefined,
+    createdAt: Date,
+  ) {
+    this.id = id;
+    this.providerType = providerType;
+    this.providerId = providerId;
+    this.credentials = credentials;
+    this.createdAt = createdAt;
+  }
+}
