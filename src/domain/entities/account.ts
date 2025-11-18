77e47c2dfeff1645d8d3ca059391363a7890dc69
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-11-18 14:30:07 +0700

    Remove name property

--- src/domain/entities/account.ts
+++ src/domain/entities/account.ts
@@ -1,19 +1,16 @@
 export class Account {
   public readonly id: string;
-  public readonly name: string;
   public readonly isActive: boolean;
   public readonly isDelete: boolean;
   public readonly createdAt: Date;
 
   constructor(
     id: string,
-    name: string,
     isActive: boolean,
     isDelete: boolean,
     createdAt: Date,
   ) {
     this.id = id;
-    this.name = name;
     this.isActive = isActive;
     this.isDelete = isDelete;
     this.createdAt = createdAt;
