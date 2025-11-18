b67451fc21105c6c0e9c7734a31846806df55dc6
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-11-19 00:29:20 +0700

    Remove id property

--- src/domain/entities/account-provider.ts
+++ src/domain/entities/account-provider.ts
@@ -4,26 +4,11 @@ export enum ProviderType {
 }
 
 export class AccountProvider {
-  public readonly id: string;
-  public readonly accountId: string;
-  public readonly providerType: ProviderType;
-  public readonly providerId: string;
-  public readonly credentials: string | null | undefined;
-  public readonly createdAt: Date;
-
   constructor(
-    id: string,
-    accountId: string,
-    providerType: ProviderType,
-    providerId: string,
-    credentials: string | null | undefined,
-    createdAt: Date,
-  ) {
-    this.id = id;
-    this.accountId = accountId;
-    this.providerType = providerType;
-    this.providerId = providerId;
-    this.credentials = credentials;
-    this.createdAt = createdAt;
-  }
+    public readonly accountId: string,
+    public readonly providerType: ProviderType,
+    public readonly providerId: string,
+    public readonly credentials: string | null | undefined,
+    public readonly createdAt: Date,
+  ) {}
 }
