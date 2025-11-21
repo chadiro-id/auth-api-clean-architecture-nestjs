d1ec8d3f725d37b9bf737e5f391bf2f85bc7bc9a
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-11-21 13:20:40 +0700

    Remove account contact

--- src/domain/repositories/account-contact.repository.ts
+++
@@ -1,6 +0,0 @@
-import { AccountContact } from '../entities/account-contact';
-
-export interface AccountContactRepository {
-  save(accountContact: AccountContact): Promise<AccountContact | null>;
-  findAllByAccountId(accountId: string): Promise<AccountContact[]>;
-}
