ce38a436d1b6807afd5213bab47d26a2671c0638
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-11-21 13:37:22 +0700

    Update account properties

--- src/domain/entities/account.ts
+++ src/domain/entities/account.ts
@@ -3,11 +3,10 @@ type Status = 'ACTIVE' | 'SUSPENDED' | 'DELETED';
 export class Account {
   constructor(
     public readonly id: string,
+    public readonly userId: string,
     public readonly password: string | null,
     public readonly status: Status,
     public readonly isDelete: boolean,
     public readonly createdAt: Date,
-    public readonly updatedAt: Date | null,
-    public readonly deletedAt: Date | null,
   ) {}
 }
