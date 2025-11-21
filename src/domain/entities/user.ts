2cd975eaac84635525b0d1cb9997fbe5b67f1efb
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-11-21 13:36:03 +0700

    Update user property

--- src/domain/entities/user.ts
+++ src/domain/entities/user.ts
@@ -1,8 +1,7 @@
 export class User {
   constructor(
     public readonly id: string,
-    public readonly username: string,
-    public readonly password: string,
+    public readonly name: string,
     public readonly fullname: string,
   ) {}
 }
