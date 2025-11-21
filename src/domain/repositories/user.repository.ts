3af569ff842a62b3f24a72542b95e6dbf3396645
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-11-21 13:40:17 +0700

    New: user repository interface

---
+++ src/domain/repositories/user.repository.ts
@@ -0,0 +1,8 @@
+import { Account } from '../entities/account';
+import { Identity } from '../entities/identity';
+import { User } from '../entities/user';
+
+export interface UserRepository {
+  save(user: User, account: Account, identity: Identity): Promise<void>;
+  findById(id: string): Promise<User | null>;
+}
