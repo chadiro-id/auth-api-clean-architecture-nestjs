a71972011e790ff0d388957ca441a10a0ace5a78
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-11-21 12:43:39 +0700

    Add password property

--- src/domain/entities/account.ts
+++ src/domain/entities/account.ts
@@ -3,6 +3,7 @@ type Status = 'ACTIVE' | 'SUSPENDED' | 'DELETED';
 export class Account {
   constructor(
     public readonly id: string,
+    public readonly password: string | null,
     public readonly status: Status,
     public readonly isDelete: boolean,
     public readonly createdAt: Date,
