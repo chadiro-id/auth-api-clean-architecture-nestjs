51b66ff319fade15aa59be741c42fcbcfd543e76
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-11-21 12:37:04 +0700

    Rename account provider to identity

--- src/domain/entities/identity.ts
+++ src/domain/entities/identity.ts
@@ -1,11 +1,10 @@
 type ProviderType = 'EMAIL' | 'GOOGLE';
 
-export class AccountProvider {
+export class Identity {
   constructor(
     public readonly accountId: string,
     public readonly providerType: ProviderType,
     public readonly providerId: string,
-    public readonly credentials: string | null,
     public readonly createdAt: Date,
   ) {}
 }
