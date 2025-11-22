50b1c33294bfd09cca0aec28de7416b930b53542
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-11-23 00:08:07 +0700

    Describe register user logic

--- src/application/use-cases/user/register-user.usecase.ts
+++ src/application/use-cases/user/register-user.usecase.ts
@@ -1,11 +1,49 @@
 import { AccountRepository } from 'src/domain/repositories/account.repository';
 import { IdentityRepository } from 'src/domain/repositories/identity.repository';
 import { UserRepository } from 'src/domain/repositories/user.repository';
+import { RegisterUserInput } from './dtos/register-user.dto';
+import { PasswordHashingService } from 'src/application/services/password-hashing.service';
+import { UniqueIdService } from 'src/application/services/unique-id.service';
+import { User } from 'src/domain/entities/user';
+import { Account } from 'src/domain/entities/account';
+import { Identity } from 'src/domain/entities/identity';
 
 export class RegisterUserUseCase {
   constructor(
     private readonly userRepository: UserRepository,
     private readonly accountRepository: AccountRepository,
     private readonly identityRepository: IdentityRepository,
+    private readonly passwordHashingService: PasswordHashingService,
+    private readonly uniqueIdService: UniqueIdService,
   ) {}
+
+  async execute(input: RegisterUserInput) {
+    const isIdentityExists = await this.identityRepository.existsByEmail(
+      input.email,
+    );
+
+    if (isIdentityExists) {
+      throw new Error('User already exists');
+    }
+
+    const userId = this.uniqueIdService.generate();
+    const accountId = this.uniqueIdService.generate();
+    const hashedPassword = await this.passwordHashingService.hash(
+      input.password,
+    );
+    const date = new Date();
+
+    const user = new User(userId, input.fullname, input.fullname);
+    const account = new Account(
+      accountId,
+      userId,
+      hashedPassword,
+      'ACTIVE',
+      false,
+      date,
+    );
+    const identity = new Identity(accountId, 'EMAIL', input.email, date);
+
+    await this.userRepository.save(user, account, identity);
+  }
 }
