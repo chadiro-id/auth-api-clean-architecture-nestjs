6a7e65843196933a096529672607c741b68df59b
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-11-23 14:07:47 +0700

    import repositories

--- src/application/use-cases/authentication/user-login.usecase.ts
+++ src/application/use-cases/authentication/user-login.usecase.ts
@@ -1,5 +1,11 @@
+import { AccountRepository } from 'src/domain/repositories/account.repository';
+import { UserRepository } from 'src/domain/repositories/user.repository';
+
 export class UserLoginUseCase {
-  constructor() {}
+  constructor(
+    private readonly accountRepository: AccountRepository,
+    private readonly userRepository: UserRepository,
+  ) {}
 
   async execute() {}
 }
