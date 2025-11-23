8de3eddbcd6847017887cbeb0342a4ba8854f2b1
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-11-23 13:47:46 +0700

    Update identities relation to user entity

--- src/domain/entities/identity.ts
+++ src/domain/entities/identity.ts
@@ -2,7 +2,7 @@ export type ProviderType = 'USERNAME' | 'EMAIL' | 'GOOGLE';
 
 export class Identity {
   constructor(
-    public readonly accountId: string,
+    public readonly userId: string,
     public readonly providerType: ProviderType,
     public readonly providerId: string,
     public readonly createdAt: Date,
