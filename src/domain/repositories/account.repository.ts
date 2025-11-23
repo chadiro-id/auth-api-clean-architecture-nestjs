45081b0bccc015424e051de0cde3c1e810665283
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-11-23 14:03:39 +0700

    Update necessary methods

--- src/domain/repositories/account.repository.ts
+++ src/domain/repositories/account.repository.ts
@@ -1,8 +1,10 @@
 import { Account } from '../entities/account';
-import { Identity } from '../entities/identity';
+import { ProviderType } from '../entities/identity';
 
 export interface AccountRepository {
-  create(account: Account, identity: Identity): Promise<void>;
-
   findById(id: string): Promise<Account | null>;
+  findByProvider(
+    providerType: ProviderType,
+    providerId: string,
+  ): Promise<Account | null>;
 }
