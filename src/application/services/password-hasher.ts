c6645fe1e4fb772304e4585e5e7c86ae04380023
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-11-23 14:17:58 +0700

    Rename password hashing service class

--- src/application/services/password-hasher.ts
+++ src/application/services/password-hasher.ts
@@ -1,4 +1,4 @@
-export interface PasswordHashingService {
+export interface PasswordHasher {
   hash(password: string): Promise<string>;
   compare(password: string, hashedPassword: string): Promise<boolean>;
 }
