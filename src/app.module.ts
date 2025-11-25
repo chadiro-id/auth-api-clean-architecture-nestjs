26f2c3e43afc683b437bd3bfdca4013621a76f1c
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-11-25 19:21:49 +0700

    Add users module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AuthenticationsModule } from './presentation/http/api/authentications/authentications.module';
+import { UsersModule } from './presentation/http/api/users/users.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), AuthenticationsModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    AuthenticationsModule,
+    UsersModule,
+  ],
   controllers: [],
   providers: [],
 })
