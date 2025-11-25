fb1829e53dfc80af421d94ed184eaedee28deaa4
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-11-25 14:38:11 +0700

    Configure database module

--- src/infrastructure/database/database.module.ts
+++ src/infrastructure/database/database.module.ts
@@ -1,22 +1,11 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule, ConfigType } from '@nestjs/config';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { DataSource, DataSourceOptions } from 'typeorm';
+import { ConfigModule } from '@nestjs/config';
+import { PgPoolProvider } from './postgres/pg-pool.provider';
 import databaseConfig from '../config/database.config';
 
 @Module({
-  imports: [
-    TypeOrmModule.forRootAsync({
-      imports: [ConfigModule.forFeature(databaseConfig)],
-      inject: [databaseConfig.KEY],
-      useFactory: (config: ConfigType<typeof databaseConfig>) => config.pg,
-      dataSourceFactory: async (options: DataSourceOptions) => {
-        const dataSource = new DataSource(options);
-        return dataSource.initialize();
-      },
-    }),
-  ],
-  providers: [],
-  exports: [],
+  imports: [ConfigModule.forFeature(databaseConfig)],
+  providers: [PgPoolProvider],
+  exports: [PgPoolProvider],
 })
 export class DatabaseModule {}
