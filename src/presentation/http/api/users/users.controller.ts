bc6f5eaa33334d9816a148a161ae1b864542b7b9
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-11-25 19:19:24 +0700

    inject usecase dependency

--- src/presentation/http/api/users/users.controller.ts
+++ src/presentation/http/api/users/users.controller.ts
@@ -1,7 +1,14 @@
-import { Controller, Post } from '@nestjs/common';
+import { Controller, Inject, Post } from '@nestjs/common';
+import { RegisterUserUseCase } from 'src/application/use-cases/user/register-user.usecase';
+import { REGISTER_USER_USE_CASE_KEY } from 'src/infrastructure/usecase/users.usecase.provider';
 
 @Controller('users')
 export class UsersController {
+  constructor(
+    @Inject(REGISTER_USER_USE_CASE_KEY)
+    private readonly registerUserUseCase: RegisterUserUseCase,
+  ) {}
+
   @Post()
   async handlePostUser() {}
 }
