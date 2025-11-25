53c038edf20ba02449e96aa14a0927b125a5f67d
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-11-25 18:50:16 +0700

    Update authentications module

--- src/presentation/http/api/authentications/authentications.module.ts
+++ src/presentation/http/api/authentications/authentications.module.ts
@@ -1,4 +1,9 @@
 import { Module } from '@nestjs/common';
+import { AuthenticationsUseCaseModule } from 'src/infrastructure/usecase/authentications.usecase.module';
+import { AuthenticationsController } from './authentications.controller';
 
-@Module({})
+@Module({
+  imports: [AuthenticationsUseCaseModule],
+  controllers: [AuthenticationsController],
+})
 export class AuthenticationsModule {}
