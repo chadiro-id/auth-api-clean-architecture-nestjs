2f46c3f70321d8a9c455a52735c5291d3d55879e
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-11-26 03:25:12 +0700

    Update property name

--- src/domain/entities/authentication.ts
+++ src/domain/entities/authentication.ts
@@ -2,7 +2,7 @@ export class Authentication {
   constructor(
     public readonly accountId: string,
     public readonly token: string,
-    public readonly expiresAt: Date,
+    public readonly expiryDate: Date,
     public readonly createdAt: Date,
   ) {}
 }
