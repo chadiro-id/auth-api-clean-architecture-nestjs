5157c2c82dde58e8b0e5393707d78b67024bf95a
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-11-25 18:55:12 +0700

    Update module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { DatabaseModule } from './infrastructure/database/database.module';
+import { AuthenticationsModule } from './presentation/http/api/authentications/authentications.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), DatabaseModule],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), AuthenticationsModule],
   controllers: [],
   providers: [],
 })
