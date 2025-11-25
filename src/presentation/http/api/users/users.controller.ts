5968a2d1522cce7b6b7afc6ce27708afdd8f6613
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-11-25 23:10:46 +0700

    Provide register user handler

--- src/presentation/http/api/users/users.controller.ts
+++ src/presentation/http/api/users/users.controller.ts
@@ -1,6 +1,14 @@
-import { Controller, Inject, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  HttpCode,
+  HttpStatus,
+  Inject,
+  Post,
+} from '@nestjs/common';
 import { RegisterUserUseCase } from 'src/application/use-cases/user/register-user.usecase';
 import { REGISTER_USER_USE_CASE_KEY } from 'src/infrastructure/usecase/users.usecase.provider';
+import { RegisterRequestDto } from './users.dto';
 
 @Controller('users')
 export class UsersController {
@@ -10,5 +18,11 @@ export class UsersController {
   ) {}
 
   @Post()
-  async handlePostUser() {}
+  @HttpCode(HttpStatus.CREATED)
+  async register(@Body() dto: RegisterRequestDto) {
+    const result = await this.registerUserUseCase.execute(dto);
+    return {
+      data: result,
+    };
+  }
 }
--- src/presentation/http/api/users/users.dto.ts
+++ src/presentation/http/api/users/users.dto.ts
@@ -1,4 +1,4 @@
-export class RegisterUserDto {
+export class RegisterRequestDto {
   email: string;
   password: string;
   fullname: string;
