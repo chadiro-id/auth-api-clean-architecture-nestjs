1d9028713d955985d6eb00d11d1b8b69a88cdf56
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-11-26 04:11:19 +0700

    Populate find by provider method

--- src/infrastructure/repository/account.repository.postgres.ts
+++ src/infrastructure/repository/account.repository.postgres.ts
@@ -18,7 +18,24 @@ export class AccountRepositoryPostgres implements AccountRepository {
     providerType: ProviderType,
     providerId: string,
   ): Promise<Account | null> {
-    console.log(providerType, providerId);
-    throw new Error('Method not implemented.');
+    const query = {
+      text: 'SELECT a.* FROM accounts a LEFT JOIN identitites i ON a.id = i.account_id WHERE i.provider_type = $1 AND i.provider_id = $2',
+      values: [providerType, providerId],
+    };
+
+    const result = await this.pool.query(query);
+    if (result.rows.length === 0) {
+      return null;
+    }
+    const row = result.rows[0];
+    return new Account(
+      row.id as string,
+      row.user_id as string,
+      row.password as string,
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+      row.status,
+      row.is_delete as boolean,
+      row.created_at as Date,
+    );
   }
 }
