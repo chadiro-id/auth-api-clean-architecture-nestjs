5d984b2748aca29451ea525287bb7d5a3c8c1257
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-11-25 18:44:35 +0700

    New: service module

---
+++ src/infrastructure/service/service.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import jwtConfig from '../config/jwt.config';
+import { BcryptPasswordHash } from './bcrypt-password-hash';
+import { JwtTokenService } from './jwt-token-service';
+import { NanoidGenerator } from './nanoid-generator';
+
+@Module({
+  imports: [ConfigModule.forFeature(jwtConfig)],
+  providers: [BcryptPasswordHash, JwtTokenService, NanoidGenerator],
+  exports: [BcryptPasswordHash, JwtTokenService, NanoidGenerator],
+})
+export class ServiceModule {}
