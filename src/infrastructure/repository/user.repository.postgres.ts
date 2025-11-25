54891d62f1d43a447a680396da15d04848886834
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-11-26 03:41:14 +0700

    Populate save aggregate method

--- src/infrastructure/repository/user.repository.postgres.ts
+++ src/infrastructure/repository/user.repository.postgres.ts
@@ -12,8 +12,46 @@ export class UserRepositoryPostgres implements UserRepository {
     account: Account,
     identity: Identity,
   ): Promise<void> {
-    console.log(user, account, identity);
-    throw new Error('Method not implemented.');
+    const inserUserQuery = {
+      text: 'INSERT INTO users VALUES ($1, $2, $3)',
+      values: [user.id, user.name, user.createdAt],
+    };
+    const insertAccountQuery = {
+      text: 'INSERT INTO accounts VALUES ($1, $2, $3, $4, $5, $6)',
+      values: [
+        account.id,
+        account.userId,
+        account.password,
+        account.status,
+        account.isDelete,
+        account.createdAt,
+      ],
+    };
+    const insertIdentityQuery = {
+      text: 'INSERT INTO identities (account_id, provider_type, provider_id, created_at) VALUES ($1, $2, $3, $4)',
+      values: [
+        identity.accountId,
+        identity.providerType,
+        identity.providerId,
+        identity.createdAt,
+      ],
+    };
+
+    const client = await this.pool.connect();
+    try {
+      await client.query('BEGIN');
+
+      await client.query(inserUserQuery);
+      await client.query(insertAccountQuery);
+      await client.query(insertIdentityQuery);
+
+      await client.query('COMMIT');
+    } catch (error) {
+      await client.query('ROLLBACK');
+      throw error;
+    } finally {
+      client.release();
+    }
   }
   async findById(id: string): Promise<User | null> {
     console.log(id);
