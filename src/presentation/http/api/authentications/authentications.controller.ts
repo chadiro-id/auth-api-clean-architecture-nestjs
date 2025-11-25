6ff018dee6d61f341a5340114f939ad9763430fc
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-11-25 18:54:56 +0700

    Inject dependencies

--- src/presentation/http/api/authentications/authentications.controller.ts
+++ src/presentation/http/api/authentications/authentications.controller.ts
@@ -1,9 +1,13 @@
-import { Controller, Post } from '@nestjs/common';
+import { Controller, Inject, Post } from '@nestjs/common';
 import { UserLoginUseCase } from 'src/application/use-cases/authentication/user-login.usecase';
+import { USER_LOGIN_USE_CASE_KEY } from 'src/infrastructure/usecase/authentications.usecase.provider';
 
 @Controller('authentications')
 export class AuthenticationsController {
-  constructor(private readonly userLoginUseCase: UserLoginUseCase) {}
+  constructor(
+    @Inject(USER_LOGIN_USE_CASE_KEY)
+    private readonly userLoginUseCase: UserLoginUseCase,
+  ) {}
 
   @Post()
   async login() {}
