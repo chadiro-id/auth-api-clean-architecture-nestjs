c8bc8d6611bc4c4f77c92fe939bb6a63286f15a5
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-11-29 00:24:58 +0700

    New: authentications provider

---
+++ src/presentation/http/api/authentications/authentications.provider.ts
@@ -0,0 +1,38 @@
+import { AuthTokenService } from 'src/application/services/auth-token-service';
+import { IdGenerator } from 'src/application/services/id-generator';
+import { PasswordHasher } from 'src/application/services/password-hasher';
+import { UserLoginUseCase } from 'src/application/use-cases/authentication/user-login.usecase';
+import { AuthenticationRepository } from 'src/domain/repositories/authentication.repository';
+import { UserRepository } from 'src/domain/repositories/user.repository';
+import { AuthenticationRepositoryPostgres } from 'src/infrastructure/repository/authentication.repository.postgres';
+import { UserRepositoryPostgres } from 'src/infrastructure/repository/user.repository.postgres';
+import { BcryptPasswordHash } from 'src/infrastructure/service/bcrypt-password-hash';
+import { JwtTokenService } from 'src/infrastructure/service/jwt-token-service';
+import { NanoidGenerator } from 'src/infrastructure/service/nanoid-generator';
+
+export const USER_LOGIN_USE_CASE_TOKEN = Symbol('USER_LOGIN_USE_CASE');
+export const UserLoginUseCaseProvider = {
+  provide: USER_LOGIN_USE_CASE_TOKEN,
+  useFactory: (
+    authenticationRepository: AuthenticationRepository,
+    userRepository: UserRepository,
+    passwordHasher: PasswordHasher,
+    authTokenService: AuthTokenService,
+    idGenerator: IdGenerator,
+  ) => {
+    return new UserLoginUseCase(
+      authenticationRepository,
+      userRepository,
+      passwordHasher,
+      authTokenService,
+      idGenerator,
+    );
+  },
+  inject: [
+    AuthenticationRepositoryPostgres,
+    UserRepositoryPostgres,
+    BcryptPasswordHash,
+    JwtTokenService,
+    NanoidGenerator,
+  ],
+};
