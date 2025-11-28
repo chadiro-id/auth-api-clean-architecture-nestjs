c2fc68fca68dcf9d81ac56e3de6931edf2269fe8
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-11-29 00:30:00 +0700

    Update usecase injection token

--- src/presentation/http/api/authentications/authentications.controller.ts
+++ src/presentation/http/api/authentications/authentications.controller.ts
@@ -7,13 +7,13 @@ import {
   Post,
 } from '@nestjs/common';
 import { UserLoginUseCase } from 'src/application/use-cases/authentication/user-login.usecase';
-import { USER_LOGIN_USE_CASE_KEY } from 'src/infrastructure/usecase/authentications.usecase.provider';
 import { LoginRequestDto } from './authentications.dto';
+import { USER_LOGIN_USE_CASE_TOKEN } from './authentications.provider';
 
 @Controller('authentications')
 export class AuthenticationsController {
   constructor(
-    @Inject(USER_LOGIN_USE_CASE_KEY)
+    @Inject(USER_LOGIN_USE_CASE_TOKEN)
     private readonly userLoginUseCase: UserLoginUseCase,
   ) {}
 
