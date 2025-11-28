f8f868575a29facf78ed137e66122dbbe3176007
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-11-28 23:17:56 +0700

    Update repo providers

--- src/infrastructure/repository/repository.module.ts
+++ src/infrastructure/repository/repository.module.ts
@@ -1,23 +1,11 @@
 import { Module } from '@nestjs/common';
 import { DatabaseModule } from '../database/database.module';
-import { AccountRepositoryPostgres } from './account.repository.postgres';
 import { AuthenticationRepositoryPostgres } from './authentication.repository.postgres';
-import { IdentityRepositoryPostgres } from './identity.repository.postgres';
 import { UserRepositoryPostgres } from './user.repository.postgres';
 
 @Module({
   imports: [DatabaseModule],
-  providers: [
-    AccountRepositoryPostgres,
-    AuthenticationRepositoryPostgres,
-    IdentityRepositoryPostgres,
-    UserRepositoryPostgres,
-  ],
-  exports: [
-    AccountRepositoryPostgres,
-    AuthenticationRepositoryPostgres,
-    IdentityRepositoryPostgres,
-    UserRepositoryPostgres,
-  ],
+  providers: [AuthenticationRepositoryPostgres, UserRepositoryPostgres],
+  exports: [AuthenticationRepositoryPostgres, UserRepositoryPostgres],
 })
 export class RepositoryModule {}
