fe4a45371cd66d112d3bc60bfd40bcaae2e95735
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-11-28 22:50:59 +0700

    Update save method user repository

--- src/domain/repositories/user.repository.ts
+++ src/domain/repositories/user.repository.ts
@@ -1,13 +1,7 @@
-import { Account } from '../entities/account';
-import { Identity } from '../entities/identity';
 import { User } from '../entities/user';
 
 export interface UserRepository {
-  saveAggregate(
-    user: User,
-    account: Account,
-    identity: Identity,
-  ): Promise<void>;
+  save(user: User): Promise<void>;
 
   findById(id: string): Promise<User | null>;
 }
