cef5862da6f26c79ff5d3c018adb43414692b5ec
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-11-28 22:49:58 +0700

    Remove unused repositories

--- src/domain/repositories/identity.repository.ts
+++
@@ -1,8 +0,0 @@
-import { Identity } from '../entities/identity';
-
-export interface IdentityRepository {
-  save(identity: Identity): Promise<void>;
-  findAllByAccountId(accountId: string): Promise<Identity[]>;
-  existsByEmail(email: string): Promise<boolean>;
-  findByEmail(email: string): Promise<Identity | null>;
-}
