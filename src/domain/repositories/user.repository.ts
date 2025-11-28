05b662a307caaecd63e745adc2134b72da4fe74d
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-11-28 23:26:05 +0700

    Add more query method

--- src/domain/repositories/user.repository.ts
+++ src/domain/repositories/user.repository.ts
@@ -2,6 +2,7 @@ import { User } from '../entities/user';
 
 export interface UserRepository {
   save(user: User): Promise<void>;
-
   findById(id: string): Promise<User | null>;
+  findByUsername(username: string): Promise<User | null>;
+  findByEmail(email: string): Promise<User | null>;
 }
