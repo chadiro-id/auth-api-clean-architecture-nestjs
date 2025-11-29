08416d6924c91156bd81e478b8505bfc79f1ea66
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-11-29 17:18:14 +0700

    Simplify usecase params

--- src/application/use-cases/get-user.usecase.ts
+++ src/application/use-cases/get-user.usecase.ts
@@ -3,8 +3,8 @@ import { UserRepository } from 'src/domain/repositories/user.repository';
 export class GetUserUseCase {
   constructor(private readonly userRepository: UserRepository) {}
 
-  async execute(identifier: string) {
-    const user = await this.userRepository.findById(identifier);
+  async execute(id: string) {
+    const user = await this.userRepository.findById(id);
     if (user === null) {
       throw new Error('User not found');
     }
